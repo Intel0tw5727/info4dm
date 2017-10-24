eac7b37c7c27f6cccf0edc115aff3be2d3086dc6
Shingo Tawada <e155727.ha1cyon@gmail.com>
2017-10-24 15:54:08 +0900

    regression.py ver.1の追加

---
+++ regression.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+class LinearRegression:
+    x = None
+    theta = None
+
+    def fit(self, x, y):
+        pass
+
+    def predict(self, x, y):
+        pass
+
+    def score(self, x, y):
+        pass
